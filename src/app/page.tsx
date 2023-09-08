ff3ce35fd1ffb14764e8d7e3b7821b329f79487c
Suad Muhic <105297253+SuadMuhic@users.noreply.github.com>
2023-09-08 17:05:29 +0200

    Created game board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Game() {
-  return <div>Game</div>;
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Link
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        href="/game"
+      >
+        Play Ludo
+      </Link>
+    </div>
+  );
 }
